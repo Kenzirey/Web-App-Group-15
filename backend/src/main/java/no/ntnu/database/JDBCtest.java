2295ffd3a99828ec5a70a69c9bbfbc685472c718
emmanes <Emma.nesvik@hotmail.com>
2024-02-15 15:15:08 +0100

    Agreed to use 4 space indent

--- backend/src/main/java/no/ntnu/database/JDBCtest.java
+++ backend/src/main/java/no/ntnu/database/JDBCtest.java
@@ -13,58 +13,58 @@ import java.util.logging.Logger;
  * Primarily used for testing a simple SELECT query.
  */
 public class JDBCtest {
-  //Connection => Statement => ResultSet.
+    //Connection => Statement => ResultSet.
 
-  private static final Logger LOGGER = Logger.getLogger(JDBCtest.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(JDBCtest.class.getName());
 
-  public static void main(String[] args) throws SQLException {
-    //test is the name of the 'test' database.
-    String url = "jdbc:mysql://localhost:3306/test";
-    //When database is on server, change this from localhost.
+    public static void main(String[] args) throws SQLException {
+        //test is the name of the 'test' database.
+        String url = "jdbc:mysql://localhost:3306/test";
+        //When database is on server, change this from localhost.
 
-    String userName = "root";
-    String password = "abcd1234";
+        String userName = "root";
+        String password = "abcd1234";
 
-    //The query via String format.
-    String query = "SELECT * FROM category";
-
-
-    //Check if the driver import works.
-    try {
-      Class.forName("com.mysql.cj.jdbc.Driver");
-    } catch (ClassNotFoundException e) {
-      LOGGER.log(Level.SEVERE, "Driver not found", e);
-    }
-
-    //Create a connection to the database
-    // () auto closes at end of catch, when used with try.
-    try (
-            Connection connection = DriverManager.getConnection(
-                    url, userName, password);
-            Statement statement = connection.createStatement();
-    ) {
-
-      //https://docs.oracle.com/javase/8/docs/api/java/sql/ResultSet.html
-      //table of data representing the result set from a database query.
-      ResultSet result = statement.executeQuery(query);
-      int entryNumber = 1;
+        //The query via String format.
+        String query = "SELECT * FROM category";
 
 
-      while (result.next()) {
-        StringBuilder testDataBuilder = new StringBuilder();
-        testDataBuilder.append("Entry ").append(entryNumber).append(": ");
-        for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
-          if (i > 1) testDataBuilder.append(", ");
-          testDataBuilder.append(result.getMetaData().getColumnName(i)).append(" = ").append(result.getString(i));
+        //Check if the driver import works.
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            LOGGER.log(Level.SEVERE, "Driver not found", e);
         }
-        System.out.println(testDataBuilder);
 
-        //Increment entry number as we want the next entry to be 'entry + 1'.
-        entryNumber++;
-      }
+        //Create a connection to the database
+        // () auto closes at end of catch, when used with try.
+        try (
+                Connection connection = DriverManager.getConnection(
+                        url, userName, password);
+                Statement statement = connection.createStatement();
+        ) {
 
-    } catch (SQLException e) {
-      LOGGER.log(Level.WARNING, "Error reading query ", e);
+            //https://docs.oracle.com/javase/8/docs/api/java/sql/ResultSet.html
+            //table of data representing the result set from a database query.
+            ResultSet result = statement.executeQuery(query);
+            int entryNumber = 1;
+
+
+            while (result.next()) {
+                StringBuilder testDataBuilder = new StringBuilder();
+                testDataBuilder.append("Entry ").append(entryNumber).append(": ");
+                for (int i = 1; i <= result.getMetaData().getColumnCount(); i++) {
+                    if (i > 1) testDataBuilder.append(", ");
+                    testDataBuilder.append(result.getMetaData().getColumnName(i)).append(" = ").append(result.getString(i));
+                }
+                System.out.println(testDataBuilder);
+
+                //Increment entry number as we want the next entry to be 'entry + 1'.
+                entryNumber++;
+            }
+
+        } catch (SQLException e) {
+            LOGGER.log(Level.WARNING, "Error reading query ", e);
+        }
     }
-  }
 }
--- backend/src/main/java/no/ntnu/database/ScriptUtility.java
+++ backend/src/main/java/no/ntnu/database/ScriptUtility.java
@@ -18,51 +18,48 @@ import java.sql.Statement;
  * Utility for running SQL scripts.
  */
 public class ScriptUtility {
-  //Script Utility template taken from:
-  //https://www.baeldung.com/java-run-sql-script
+    //Script Utility template taken from:
+    //https://www.baeldung.com/java-run-sql-script
 
 
-  //https://docs.spring.io/spring-framework/reference/testing/testcontext-framework/executing-sql.html
+    //https://docs.spring.io/spring-framework/reference/testing/testcontext-framework/executing-sql.html
 
-  public static void runScript(String path, Connection connection) {
-    //To support multiple comment delimiters.
-    boolean continueOrError = false;
-    boolean ignoreFailedDrops = false;
-    String commentPrefix = "--";
-    String separator = ";";
-    String blockCommentStartDelimiter = "/*";
-    String blockCommentEndDelimiter = "*/";
+    public static void runScript(String path, Connection connection) {
+        //To support multiple comment delimiters.
+        boolean continueOrError = false;
+        boolean ignoreFailedDrops = false;
+        String commentPrefix = "--";
+        String separator = ";";
+        String blockCommentStartDelimiter = "/*";
+        String blockCommentEndDelimiter = "*/";
 
-    ScriptUtils.executeSqlScript(
-            connection,
-            new EncodedResource(new PathResource(path)),
-            continueOrError,
-            ignoreFailedDrops,
-            commentPrefix,
-            separator,
-            blockCommentStartDelimiter,
-            blockCommentEndDelimiter
-    );
-  }
+        ScriptUtils.executeSqlScript(
+                connection,
+                new EncodedResource(new PathResource(path)),
+                continueOrError,
+                ignoreFailedDrops,
+                commentPrefix,
+                separator,
+                blockCommentStartDelimiter,
+                blockCommentEndDelimiter
+        );
+    }
 
-  /**
-   * Returns a datasource to the test database in MySQL.
-   *
-   * @return dataSource to the test database in MySQL.
-   */
-  public static DataSource getMySQLDataSourceTest() {
-    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    /**
+     * Returns a datasource to the test database in MySQL.
+     *
+     * @return dataSource to the test database in MySQL.
+     */
+    public static DataSource getMySQLDataSourceTest() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-    dataSource.setUrl("jdbc:mysql://localhost:3306/test");
-    dataSource.setUsername("root");
-    dataSource.setPassword("abcd1234");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/test");
+        dataSource.setUsername("root");
+        dataSource.setPassword("abcd1234");
 
-    return dataSource;
-  }
-
-
-
+        return dataSource;
+    }
 
 
 }
