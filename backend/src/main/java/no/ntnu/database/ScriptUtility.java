f8bd6a4b5866aa5dd9bbba177e3a37a5cb641e3a
emmanes <Emma.nesvik@hotmail.com>
2024-02-16 14:05:52 +0100

    Added javadoc to script utility.
    Thanks for reminding me, had forgotten to document the template code from baeldung.
    Note the @link way you can hyperlink the javadoc. Very neat

--- backend/src/main/java/no/ntnu/database/ScriptUtility.java
+++ backend/src/main/java/no/ntnu/database/ScriptUtility.java
@@ -10,7 +10,13 @@ import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 
 /**
- * Utility for running SQL scripts.
+ * Utility for running SQL scripts on a database.
+ * Contains methods to execute SQL scripts from a given path,
+ * using a database connection.
+ *
+ * <p> Script execution supports custom comment delimiters, script statement separators,
+ * as well as the option to continue
+ * execution on error or to ignore failed drops.</p>
  */
 public class ScriptUtility {
 	//Script Utility template taken from:
@@ -24,10 +30,12 @@ public class ScriptUtility {
 	}
 
 	/**
-	 * TODO: Someone made a javadoc-less methond, git blame Emma :).
+	 * Executes an SQL script located at the specified path using the provided {@link Connection}.
+	 * This method allows the execution of SQL scripts with custom settings for handling comments,
+	 * statement separators, and block comments.
 	 *
-	 * @param path TODO: document what this parameter is
-	 * @param connection TODO: document what this parameter is
+	 * @param path       the file system path to the SQL script file.
+	 * @param connection the {@link Connection} to the database where the script will be executed.
 	 */
 	public static void runScript(String path, Connection connection) {
 		//To support multiple comment delimiters.
@@ -51,9 +59,10 @@ public class ScriptUtility {
 	}
 
 	/**
-	 * Returns a datasource to the test database in MySQL.
+	 * Creates and returns a datasource to a test database in MySQL (localhost).
+	 * Configured via JDBC connection details for the test database.
 	 *
-	 * @return dataSource to the test database in MySQL.
+	 * @return a {@link DataSource} configured dataSource for accessing the test database.
 	 */
 	public static DataSource getMySqlDataSourceTest() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
