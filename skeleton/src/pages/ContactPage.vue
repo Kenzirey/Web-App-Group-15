d45b7b51e4df13ec9570e17e15d26f8d1e72350b
Tony Nhat Huy Pham <Tony.n.h.pham@hotmail.com>
2024-02-20 20:00:18 +0100

    Contact us 1v
    contact css

--- skeleton/src/pages/ContactPage.vue
+++ skeleton/src/pages/ContactPage.vue
@@ -9,16 +9,29 @@
         </section>
 
     
-        <section>
-            <div class="box"> Report Issues</div>
+        <section id="reportIssues">
+            <div class="box">
+                <div class="topLeftText">
+                    Report issues
+                </div>
+                <p class="pContact"> If there are any issues with our webside, feel free to send a report to EMAIL</p>
+                <!--TODO: Legg til en emial, som brukeren kan trykke på som viderfører den til eposten sin. Diskuter med gruppa -->
+            </div>
+        </section>
+
+        <section id="adresses">
+            <div class="box">
+                <div class="topLeftText">
+                    Contact us :D
+                </div>
+                <p class="pContact"> If you want to contact us  </p>
+                <!--TODO legg til kontakter diskuterer med gruppa -->
+            </div>
         </section>
     
-
-
-
-
-
-
-
     </div>
 </template>
+
+<style scoped>
+    @import '../assets/contact.css';
+</style>
