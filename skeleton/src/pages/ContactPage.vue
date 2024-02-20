5f1396f030bcb5c3f70972b35402537295b5f9d2
Tony Nhat Huy Pham <Tony.n.h.pham@hotmail.com>
2024-02-20 14:55:52 +0100

    Contact page v.0.1

---
+++ skeleton/src/pages/ContactPage.vue
@@ -0,0 +1,24 @@
+<!-- src/pages/ConatctPage.vue -->
+
+<template>
+    <div>
+        <section id="contactUsOverHead">
+            <h2>Contact us!</h2>
+            <p> If there is anything you are wondering about 
+                ,or have any input on our website, we would love to hear from you!</p>
+        </section>
+
+    
+        <section>
+            <div class="box"> Report Issues</div>
+        </section>
+    
+
+
+
+
+
+
+
+    </div>
+</template>
