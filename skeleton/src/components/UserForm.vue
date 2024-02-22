19778881ddb77a194b1e99c91e82b3b1151dbd55
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-02-22 16:22:43 +0100

    More forms

---
+++ skeleton/src/components/UserForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <div>
+      <label for="name">Name:</label>
+      <input id="name" v-model="user.name" required>
+    </div>
+    <div>
+      <label for="email">Email:</label>
+      <input id="email" type="email" v-model="user.email" required>
+    </div>
+    <div>
+      <label for="role">Role:</label>
+      <select id="role" v-model="user.role">
+        <option value="admin">Admin</option>
+        <option value="user">User</option>
+      </select>
+    </div>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'UserForm',
+  props: {
+    initialUser: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      user: this.initialUser,
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit('user-submitted', this.user);
+      this.user = { name: '', email: '', role: '' };
+    }
+  },
+};
+</script>
