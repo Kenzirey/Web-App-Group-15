d1a20c4f6d90ac936973c2bdabdc14d64f62409b
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-02-22 15:34:42 +0100

    Will be used as dto

---
+++ backend/src/main/java/no/ntnu/dto/Course.java
@@ -0,0 +1,41 @@
+package no.ntnu.dto;
+
+public class Course {
+    private String name;
+    private String description;
+    private int createdBy;
+
+    public Course() {
+    }
+
+    public Course(String name, String description, int createdBy) {
+        this.name = name;
+        this.description = description;
+        this.createdBy = createdBy;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+}
