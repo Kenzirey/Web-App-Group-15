1e6b3e3ffee58c73c1255a00c5627466245044f3
Tony Nhat Huy Pham <Tony.n.h.pham@hotmail.com>
2024-02-22 10:01:11 +0100

    ContactPage

--- skeleton/src/pages/ContactPage.vue
+++ skeleton/src/pages/ContactPage.vue
@@ -3,28 +3,33 @@
 <template>
     <div>
         <section id="contactUsOverHead">
-            <h2>Contact us!</h2>
+            <h2 class="space">Contact us!</h2>
             <p> If there is anything you are wondering about 
                 ,or have any input on our website, we would love to hear from you!</p>
         </section>
 
     
         <section id="reportIssues">
-            <div class="box">
+            <div class="box2">
                 <div class="topLeftText">
                     Report issues
                 </div>
-                <p class="pContact"> If there are any issues with our webside, feel free to send a report to EMAIL</p>
-                <!--TODO: Legg til en emial, som brukeren kan trykke på som viderfører den til eposten sin. Diskuter med gruppa -->
+                <p class="pContact"> If there are any issues with our website, feel free to send a report to our
+                    <a href="mailto:Tnhpham@stud.ntnu.no">email </a> or directly to those who worked on the website
+                </p>
             </div>
         </section>
 
         <section id="adresses">
-            <div class="box">
+            <div class="box2">
                 <div class="topLeftText">
                     Contact us :D
                 </div>
-                <p class="pContact"> If you want to contact us  </p>
+                <p class="pContact">  Address: Larsgårdsvegen 2, 6009 Ålesund <br>
+                
+                
+                </p>
+
                 <!--TODO legg til kontakter diskuterer med gruppa -->
             </div>
         </section>
