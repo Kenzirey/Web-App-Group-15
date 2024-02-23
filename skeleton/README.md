f1bee10736a0dd83989232a1a03a31bc1647b4b1
emmanes <Emma.nesvik@hotmail.com>
2024-02-23 11:06:24 +0100

    Small button change, and readme.
    Just a minor change with buttons, will figure out how to use toolbar component later, as well as fill the course website.

--- skeleton/README.md
+++ skeleton/README.md
@@ -1,3 +1,9 @@
+in the project-skeleton/
+- npm install
+- npm install axios
+- Add more things we need to remember, to successfully run the website.
+
+
 # Vue 3 + Vite
 
 This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
--- skeleton/src/components/TopRightIcons.vue
+++ skeleton/src/components/TopRightIcons.vue
@@ -1,8 +1,8 @@
 <template>
     <v-app-bar app fixed dense>
       <v-spacer></v-spacer> <!-- This pushes the content to the right -->
-      <v-btn icon="mdi-account" size="large" @click="navigateToAccount"></v-btn>
-    <v-btn icon="mdi-heart" size="large" @click="navigateToFavorites"></v-btn>
+      <v-btn prepend-icon="mdi-account" stacked size="small" @click="navigateToAccount">View Account</v-btn>
+      <v-btn prepend-icon="mdi-heart" stacked size="small" @click="navigateToFavorites">View Favorites</v-btn>
       
     </v-app-bar>
   </template>
