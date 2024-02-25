90802fb7cf9a80fa27a160bbb8385afbb976970f
emmanes <Emma.nesvik@hotmail.com>
2024-02-25 18:31:16 +0100

    treeshaking / autoimport is not working.
    Will be looking into this later, but for now am manually import as it is the onyl way I can seem to get the vuetify or vue components to work.

--- skeleton/src/plugins/vuetify.js
+++ skeleton/src/plugins/vuetify.js
@@ -2,9 +2,12 @@ import { createVuetify } from 'vuetify'
 import 'vuetify/styles'
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import { VApp, VBtn, VTextField } from 'vuetify/components'
+import { VApp, VBtn, VTextField, VAppBar, 
+  VTextarea, VRow, VCol, VContainer, VSpacer, VCarousel, VWindow } from 'vuetify/components'
 
-
+/*
+https://vuetifyjs.com/en/features/treeshaking/#automatic-treeshaking
+*/
 
 export default createVuetify({
   icons: {
@@ -18,6 +21,15 @@ export default createVuetify({
     directives,
     VApp,
     VBtn,
-    VTextField
+    VTextField,
+    VAppBar,
+    VTextarea,
+    VRow,
+    VCol,
+    VContainer,
+    VSpacer,
+    VCarousel,
+    VWindow
+
   }
 })
