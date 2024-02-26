7ed320723e1e13fef6999ba9c111acb32400bea9
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-02-26 01:33:24 +0100

    A start

---
+++ skeleton/src/components/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit.prevent="submitLogin">
+      <input type="text" v-model="username" placeholder="Username" required>
+      <input type="password" v-model="password" placeholder="Password" required>
+      <button type="submit">Login</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      async submitLogin() {
+        // TODO: Validate credentials and trigger 2FA token generation
+      }
+    }
+  };
+  </script>
+  
---
+++ skeleton/src/components/TwoFactorAuthForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <form @submit.prevent="submitToken">
+      <input type="text" v-model="token" placeholder="2FA Token" required>
+      <button type="submit">Verify</button>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        token: ''
+      };
+    },
+    methods: {
+      async submitToken() {
+        // TODO: Verify the 2FA token
+      }
+    }
+  };
+  </script>
+  
