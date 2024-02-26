85800de387e4411d1f1b640bc6dc5676b11cfc42
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-02-26 01:29:17 +0100

    Updated to support the 2FA implementation

--- backend/src/main/java/no/ntnu/database/DatabaseManager.java
+++ backend/src/main/java/no/ntnu/database/DatabaseManager.java
@@ -1,11 +1,13 @@
 package no.ntnu.database;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 import no.ntnu.database.sqlfunction.SqlConsumer;
+import no.ntnu.database.sqlfunction.SqlFunction;
 
 /**
  * Manager for the database, serves as an intermediary between the API & the database.
@@ -161,9 +163,126 @@ public class DatabaseManager {
 		);
 	}
 
+	/**
+	 * Executes a database update operation such as INSERT, UPDATE, or DELETE.
+	 *
+	 * @param query The enumeration of the SQL query to be executed.
+	 * @param prepareHook A functional interface to set the parameters of the PreparedStatement.
+	 * @return The number of rows affected by the operation.
+	 * @throws SQLException If an error occurs during the database update execution.
+	 */
 	public int executeUpdate(Query query, SqlConsumer<PreparedStatement> prepareHook) throws SQLException {
+
 		String sql = query.getString();
 		return connector.executeUpdate(sql, prepareHook);
-}
+	}
+	
+	
 
+	/**
+	 * Stores the 2FA secret key for a user in the database.
+	 *
+	 * @param username The username for which the 2FA secret key is to be saved.
+	 * @param secretKey The secret key to be stored.
+	 * @throws SQLException If an error occurs during the database update operation.
+	 */
+	public void saveUser2FASecretKey(String username, String secretKey) throws SQLException {
+        String sql = Query.INSERT_2FA_SECRET.getString();
+        executeUpdate(sql, statement -> {
+            statement.setString(1, username);
+            statement.setString(2, secretKey);
+        });
+    }
+	
+	/**
+	 * Retrieves the 2FA secret key for a user from the database.
+	 *
+	 * @param username The username for which the 2FA secret key is to be retrieved.
+	 * @return The retrieved secret key or null if not found.
+	 * @throws SQLException If an error occurs during the database query operation.
+	 */
+
+    public String get2FASecretKey(String username) throws SQLException {
+        String sql = Query.GET_2FA_SECRET.getString();
+        return executeQuery(sql, statement -> statement.setString(1, username), resultSet -> {
+            if (resultSet.next()) {
+                return resultSet.getString("two_factor_secret");
+            }
+            return null;
+        });
+    }
+
+		
+	/**
+	 * Updates the 2FA enabled status for a user in the database.
+	 *
+	 * @param username The username for which the 2FA status is to be updated.
+	 * @param isEnabled The new 2FA enabled status to be set.
+	 * @throws SQLException If an error occurs during the database update operation.
+	 */
+
+    public void set2FAEnabled(String username, boolean isEnabled) throws SQLException {
+        String sql = Query.SET_2FA_ENABLED.getString();
+        executeUpdate(sql, statement -> {
+            statement.setBoolean(1, isEnabled);
+            statement.setString(2, username);
+        });
+    }
+
+		
+	/**
+	 * Retrieves the 2FA enabled status for a user from the database.
+	 *
+	 * @param username The username for which the 2FA status is to be checked.
+	 * @return The 2FA enabled status.
+	 * @throws SQLException If an error occurs during the database query operation.
+	 */
+    public boolean get2FAEnabled(String username) throws SQLException {
+        String sql = Query.GET_2FA_ENABLED.getString();
+        return executeQuery(sql, statement -> statement.setString(1, username), resultSet -> {
+            if (resultSet.next()) {
+                return resultSet.getBoolean("is_2fa_enabled");
+            }
+            return false;
+        });
+    }
+
+	/**
+	 * Creates the 2FA table in the database if it does not exist.
+	 *
+	 * @throws SQLException If an error occurs during the database update operation.
+	 */
+    public void create2FATable() throws SQLException {
+        String sql = Query.CREATE_USERS_2FA_TABLE.getString();
+        executeUpdate(sql, PreparedStatement::execute);
+    }
+
+
+	/**
+	 * Executes an update operation with the given SQL string.
+	 *
+	 * @param sql The SQL statement to execute as a String.
+	 * @param prepareStatement A consumer that sets the parameters of the PreparedStatement.
+	 * @return The number of rows affected by the execution.
+	 * @throws SQLException If an error occurs while executing the statement.
+	 */
+	public int executeUpdate(String sql, SqlConsumer<PreparedStatement> prepareStatement) throws SQLException {
+
+        return this.connector.executeUpdate(sql, prepareStatement);
+    }
+    
+
+	/**
+	 * Executes a query operation with the given SQL string.
+	 *
+	 * @param sql The SQL query to execute as a String.
+	 * @param prepareStatement A consumer that sets the parameters of the PreparedStatement.
+	 * @param handleResultSet A function to process the ResultSet.
+	 * @param <T> The return type of the result processing function.
+	 * @return The processed result.
+	 * @throws SQLException If an error occurs while executing the query.
+	 */
+	public <T> T executeQuery(String sql, SqlConsumer<PreparedStatement> prepareStatement, SqlFunction<ResultSet, T> handleResultSet) throws SQLException {
+		return this.connector.executeQuery(sql, prepareStatement, handleResultSet);
+	}
 }
