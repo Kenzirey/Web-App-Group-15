e991d43582f02ec798ac8f345e835f0159bd1d7c
Tony Nhat Huy Pham <Tony.n.h.pham@hotmail.com>
2024-02-29 10:32:00 +0100

    FormsNyeFields

--- skeleton/src/pages/FormsPage.vue
+++ skeleton/src/pages/FormsPage.vue
@@ -2,14 +2,14 @@
 
 <template>
   <form class="formBox">
-    <fieldset>
+    <fieldset class="fieldBox">
       <h1 class="formSections">Personal information</h1>
 
       <div class="flex">
         <div class="inputGroup">
           <label class="labelNameSide">First Name:</label>
           <input
-            class="userInputSide"
+            class="userInput"
             type="First name"
             required
             v-model="firstName"
@@ -31,13 +31,13 @@
       <div class="flex">
         <div class="inputGroup">
           <label class="labelNameSide">E-mail:</label>
-          <input class="userInputSide" type="E-mail" required v-model="email" />
+          <input class="userInput" type="E-mail" required v-model="email" />
         </div>
 
         <div class="inputGroup">
-          <label class="labelNameSide">Phone Number:</label>
+          <label class="labelName">Phone Number:</label>
           <input
-            class="userInputSide"
+            class="userInput"
             type="Phone number"
             required
             v-model="phoneNumber"
@@ -64,12 +64,50 @@
       </v-container>
     </fieldset>
 
-    <fieldset>
+    <fieldset class="fieldBox">
+      <h1 class="formSections">Address</h1>
+
+      <label class="labelName">Street Address:</label>
+      <input
+        class="userInputLong"
+        type="Street Address"
+        required
+        v-model="streetAddress"
+      />
+
+      <div class="flex">
+        <div class="inputGroup">
+          <label class="labelName">City:</label>
+          <input class="userInput" type="City" required v-model="City"/>
+        </div>
+
+        <div class="inputGroup">
+          <label class="labelName">PostCode:</label>
+          <input
+            class="userInput"
+            type="PostCode"
+            required
+            v-model="Postcode"
+          />
+        </div>
+      </div>
+
+      <!-- TODO legg til land der brukeren bor,dette gidder jeg ikke nå lol-->
+      <v-select
+        class="selectFormat"
+        v-model="select"
+        :items="Country"
+        label="Country"
+        required
+      ></v-select>
+    </fieldset>
+
+    <fieldset class="fieldBox">
       <h2 class="formSections">Education Background</h2>
       <p class="labelParagraph">List your previous schools</p>
       <label class="labelName">HighSchool address</label>
       <input
-        class="userInput"
+        class="userInputLong"
         type="HighSchool"
         required
         v-model="highschool"
@@ -77,7 +115,7 @@
 
       <label class="labelName">Date graduated</label>
       <input
-        class="userInput"
+        class="userInputLong"
         type="Date graduated"
         required
         v-model="dateGraduated"
@@ -85,40 +123,40 @@
     </fieldset>
 
     <!--Temporary name finner på noe senere enn Application Details-->
-    <fieldset>
+    <fieldset class="fieldBox">
       <h3 class="formSections">Application Details</h3>
       <p class="labelParagraph">Information about the course</p>
 
-      <label class="labelName">Course Name</label>
+      <label class="labelName">Course Name:</label>
       <!--Få denne til å lese fra databasen?-->
       <input
-        class="userInput"
+        class="userInputLong"
         type="Course Name"
         required
         v-model="courseName"
       />
 
-      <label class="labelName">University</label>
+      <label class="labelName">University:</label>
       <input
-        class="userInput"
+        class="userInputLong"
         type="University"
         required
         v-model="university"
       />
 
-      <label class="labelName">Course proivder</label>
+      <label class="labelName">Course provider:</label>
       <input
-        class="userInput"
+        class="userInputLong"
         type="Course proivder"
         required
         v-model="courseProivder"
       />
 
-      <label class="labelName">Course id</label>
-      <input class="userInput" type="Course idy" required v-model="courseId" />
+      <label class="labelName">Course id:</label>
+      <input class="userInputLong" type="Course id" required v-model="courseId" />
     </fieldset>
 
-    <fieldset>
+    <fieldset class="fieldBox">
       <h4 class="formSections">Additional information</h4>
       <div class="input-group">
         <textarea
