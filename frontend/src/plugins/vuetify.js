953dc4639d60abe34cf3ea1498155d13548a9997
emmanes <Emma.nesvik@hotmail.com>
2024-04-02 18:08:40 +0200

    minor color changes, temporary. For testing

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -14,41 +14,18 @@ import { createVuetify } from 'vuetify'
 const lightTheme = {
   dark: false,
   colors: {
-    background: '#F5F5F5',
-    App: '#F5F5F5',
-    app: '#F5F5F5',
-    surface: '#F5F5F5',
-    'surface-bright': '#FFFFFF',
-    'surface-light': '#EEEEEE',
-    'surface-variant': '#424242',
-    'on-surface-variant': '#EEEEEE',
-    primary: '#1867C0',
-    'primary-darken-1': '#1F5592',
-    secondary: '#48A9A6',
-    'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00',
-    hover: '#0000FF'
+    primary: '#009688',
+    background: '#F5F5F5',  //Background of main content area
+    surface: '#F5F5F5',     //Background of the toolbar
+    secondary: '#4caf50',
+    accent: '#2196f3',
+    error: '#f44336',
+    warning: '#ff9800',
+    info: '#03a9f4',
+    success: '#8bc34'
   },
   variables: {
     'border-color': '#000000',
-    'border-opacity': 0.12,
-    'high-emphasis-opacity': 0.87,
-    'medium-emphasis-opacity': 0.60,
-    'disabled-opacity': 0.38,
-    'idle-opacity': 0.04,
-    'hover-opacity': 0.04,
-    'focus-opacity': 0.12,
-    'selected-opacity': 0.08,
-    'activated-opacity': 0.12,
-    'pressed-opacity': 0.12,
-    'dragged-opacity': 0.08,
-    'theme-kbd': '#212529',
-    'theme-on-kbd': '#FFFFFF',
-    'theme-code': '#F5F5F5',
-    'theme-on-code': '#000000',
   }
 }
 
