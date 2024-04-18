165831228b4bfeb4f72ac2249482b03d274c67a2
SmileyFace799 <Henrikgran85@gmail.com>
2024-04-18 15:32:27 +0200

    Fixed broken color

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -24,7 +24,7 @@ const lightTheme = {
     error: '#f44336',
     warning: '#ff9800',
     info: '#03a9f4',
-    success: '#8bc34' //TODO: Make this a valid hex color
+    success: '#8bc34A'
   },
   variables: {
     'border-color': '#000000',
