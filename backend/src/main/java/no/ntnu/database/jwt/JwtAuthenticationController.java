bc6be5e23f56b8c31aed96bf69a3a2b8446a3b07
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-04-19 20:36:02 +0200

    Imports because of moving the classes...

--- backend/src/main/java/no/ntnu/database/jwt/JwtAuthenticationController.java
+++ backend/src/main/java/no/ntnu/database/jwt/JwtAuthenticationController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import no.ntnu.dto.AuthenticationRequest;
+import no.ntnu.dto.AuthenticationResponse;
 import no.ntnu.security.SecurityUtil;
 
 /**
