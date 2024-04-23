8a712c3b85cbb09a05bafed67dd06a910e8be353
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-04-23 22:56:29 +0200

    Export methods to use in store.js

--- frontend/src/utility/cookieHelper.js
+++ frontend/src/utility/cookieHelper.js
@@ -4,7 +4,7 @@
  * @param cname Cookie name (key)
  * @returns {string} Value of the cookie or "" if cookie not found
  */
-function getCookie(cname) {
+export function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
@@ -27,7 +27,7 @@ function getCookie(cname) {
    * @param cvalue Value of the cookie
    * @param exdays expiry time in days
    */
-  function setCookie(cname, cvalue, exdays) {
+  export function setCookie(cname, cvalue, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
@@ -38,7 +38,7 @@ function getCookie(cname) {
    * Delete a cookie
    * @param cookieName Name of the cookie to delete
    */
-  function deleteCookie(cookieName) {
+  export function deleteCookie(cookieName) {
     setCookie(cookieName, "", -1);
   }
   
