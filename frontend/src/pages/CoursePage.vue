40d1a6359548218f38352f9d3ca2d7f81ae3269f
emmanes <Emma.nesvik@hotmail.com>
2024-04-26 14:19:52 +0200

    Formated and added v-for providers
    As we have a variety of amounts of providers per course, added a v-for to take this into account so we can use javascript data to spit the data we need from database in the future

--- frontend/src/pages/CoursePage.vue
+++ frontend/src/pages/CoursePage.vue
@@ -1,39 +1,46 @@
 <template>
-  <v-container class="course-page" fluid>
-    <div class="grid-container">
-      <h3 class="course-title">{{ course.title }}</h3>
-      <div class="session-date"><span class="key">Course Session:</span> <span class="value">{{ course.sessionDate }}</span></div>
-      <div class="content-container">
-        <div class="course-info">
-          <div class="info-box-left">
-            <div class="info-container" style="display: inline-flex; flex-direction: column;">
-              <div class="info-item"><span class="key">Course Providers:</span> <span class="value">NTNU</span></div>
-              <div class="info-item"><span class="key">Difficulty Level:</span> <span class="value">Beginner</span></div>
-              <div class="info-item"><span class="key">Course Size:</span> <span class="value">ECTs Credits</span></div>
+  <div class="grid-container">
+    <h3 class="course-title">{{ course.title }}</h3>
+    <div class="session-date"><span class="key">Course Session:</span> <span class="value">{{ course.sessionDate
+        }}</span></div>
+
+    <div class="content-container">
+      <div class="course-info">
+        <div class="info-box-left">
+          <div class="info-container" style="display: inline-flex; flex-direction: column;">
+            <div class="info-item"><span class="key">Difficulty Level:</span> <span class="value">Beginner</span></div>
+            <div class="info-item"><span class="key">Course Size:</span> <span class="value">ECTs Credits</span></div>
+            <div class="info-item" v-for="(provider, index) in course.providers" :key="index">
+              <span class="key">Provider:</span>
+              <span class="value">{{ provider.name }}</span>
             </div>
+
+
           </div>
-          <div class="info-item-separator"></div>
-          <div class="info-box-right">
-            <div class="info-container">
-              <div class="info-item"><span class="key">Hours per week:</span> <span class="value">4</span></div>
-              <div class="info-item"><span class="key">Related Certifications:</span> <span class="value">SQL Wizard</span></div>
-              <div class="info-item"><span class="key">Course Cost:</span> <span class="value">$500</span></div>
+        </div>
+        <div class="info-item-separator"></div>
+        <div class="info-box-right">
+          <div class="info-container">
+            <div class="info-item"><span class="key">Hours per week:</span> <span class="value">4</span></div>
+            <div class="info-item"><span class="key">Related Certifications:</span> <span class="value">SQL
+                Wizard</span></div>
+            <div class="info-item" v-for="(provider, index) in course.providers" :key="'cost' + index">
+              <span class="key">Cost:</span>
+              <span class="value">${{ provider.cost }}</span>
             </div>
+
           </div>
-          
+
         </div>
-        <div class="course-image">
-          <img src="/images/AWS.png" alt="AWS Course Image">
-        </div>
+
       </div>
-      <div class="course-description">{{ course.description }}</div>
+      <v-btn text="Order Course" type="apply" href="/forms" variant="outlined"></v-btn>
+      <div class="course-image">
+        <img src="/images/AWS.png" alt="AWS Course Image">
+      </div>
     </div>
-
-    <courseCard></courseCard>
-  
-
-
-</v-container>
+    <div class="course-description">{{ course.description }}</div>
+  </div>
 </template>
 
 <script>
@@ -48,9 +55,13 @@ export default {
   data() {
     return {
       course: {
-          title: 'SQL for Beginners',
-          sessionDate: '2024-04-25', // Example date
-          description: 'This is a detailed description of the course. :3 in theory, if we actually had content to put here',
+        title: 'SQL for Beginners',
+        sessionDate: '2024-04-25', // Example date
+        description: 'This is a detailed description of the course. :3 in theory, if we actually had content to put here',
+        providers: [
+          { name: 'NTNU', cost: 500 },
+          { name: 'UiO', cost: 501 }
+        ]
       },
     };
   }
@@ -58,85 +69,85 @@ export default {
 
 
 </script>
-  
+
 <style lang="scss" scoped>
-  
 /* The items, have 2 columns to break up the information
  */
-  .session-date {
-    text-align: center;
-    font-size: 1.2em;
-    margin-bottom: 15px;
-  }
-  .grid-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-  }
+.session-date {
+  text-align: center;
+  font-size: 1.2em;
+}
 
-  .course-title {
-    text-align: center;
-    font-size: 1.5em; 
-  }
+.grid-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
 
-  .content-container {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-  }
+.course-title {
+  text-align: center;
+  font-size: 1.5em;
+}
 
-  .course-info {
-    display: grid;
-    gap: 10px; 
-    width: 100%;
-    grid-template-columns: minmax(0, 1fr) calc(10vw - 80px) minmax(0, 1fr);
-  }
+.content-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
 
-  .info-item-separator {
-    grid-row: 1;
-  }
+.course-info {
+  display: grid;
+  gap: 10px;
+  width: 100%;
+  grid-template-columns: minmax(0, 1fr) calc(10vw - 80px) minmax(0, 1fr);
+}
 
-  .info-box-left {
-    display: flex;
-    grid-row: 1;
-    justify-content: end;
-  }
+.info-item-separator {
+  grid-row: 1;
+}
 
-  .info-box-right {
-    display: flex;
-    grid-row: 1;
-    justify-content: start;
-  }
+.info-box-left {
+  display: flex;
+  grid-row: 1;
+  justify-content: end;
+}
 
-  .info-container {
-    display: inline-flex;
-    flex-direction: column;
-  }
+.info-box-right {
+  display: flex;
+  grid-row: 1;
+  justify-content: start;
+}
 
-  .info-item {
-    display: flex;
-    justify-content: flex-start;
-  }
+.info-container {
+  display: inline-flex;
+  flex-direction: column;
+}
 
-  .key {
-    font-weight: bold;
-    margin-right: 8px; /* Adjust the space between key and value */
-  }
+.info-item {
+  display: flex;
+  justify-content: flex-start;
+}
 
-  .course-title, .course-description {
-      margin-top: 5px;
-      width: 100%;
-      text-align: center;
-  }
+.key {
+  font-weight: bold;
+  margin-right: 8px;
+  /* Adjust the space between key and value */
+}
 
-  .course-image img {
-  
-    min-width: 30%;
-    max-width:50%;
-    height: auto;
-  }
+.course-title,
+.course-description {
+  margin-top: 5px;
+  width: 100%;
+  text-align: center;
+}
+
+.course-image img {
+
+  min-width: 30%;
+  max-width: 50%;
+  height: auto;
+}
 </style>
-    
