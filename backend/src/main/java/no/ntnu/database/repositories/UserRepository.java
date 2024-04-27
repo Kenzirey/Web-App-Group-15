417a035079c073cd44d0a4fac936480d81310e89
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-04-28 00:56:44 +0200

    Used to display number of users with 2fa enabled in admin frontend

--- backend/src/main/java/no/ntnu/database/repositories/UserRepository.java
+++ backend/src/main/java/no/ntnu/database/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package no.ntnu.database.repositories;
 
 import no.ntnu.database.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.Optional;
 
@@ -12,4 +14,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByUsername(String username);
 	
 	boolean existsByUsername(String username);
+
+    @Query("SELECT COUNT(u) FROM users u WHERE u.isTwoFactorEnabled = true")
+    long countByTwoFactorEnabledTrue();
 }
