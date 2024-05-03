71812524884019e8c629d8ea1e1d7cb149a7ffc0
emmanes <Emma.nesvik@hotmail.com>
2024-05-03 18:51:01 +0200

    Fixed Mobile Course Page bug
    Fixed the bug where the image caused some weird background and content problems.
    
    updated the course app with new layout.
    To work better with longer text, things are now vertical

--- frontend/src/pages/CoursePage.vue
+++ frontend/src/pages/CoursePage.vue
@@ -1,59 +1,53 @@
 <template>
-  <div class="grid-container">
+  <main class="course-container">
     <h3 class="course-title">{{ course.title }}</h3>
     <div class="session-date"><span class="key">Course Session:</span> <span class="value">{{ course.sessionDate
         }}</span></div>
 
     <div class="content-container">
       <div class="course-info">
-        <div class="info-box-left">
-          <div class="info-container">
-            <div class="info-item"><span class="key">Difficulty Level:</span> <span class="value">{{ course.level
-                }}</span></div>
-            <div class="info-item"><span class="key">Course Size:</span> <span class="value">{{ course.credits }}</span>
-            </div>
-            <div class="info-item" v-for="(provider, index) in course.providers" :key="index">
-              <span class="key">Provider:</span>
-              <span class="value">{{ provider.name }}</span>
-            </div>
-
-
+        <!-- Info Container for all course details -->
+        <div class="info-container">
+          <div class="info-item">
+            <span class="key">Difficulty Level:</span>
+            <span class="value">{{ course.level }}</span>
+          </div>
+          <div class="info-item">
+            <span class="key">Course Size:</span>
+            <span class="value">{{ course.credits }}</span>
+          </div>
+          <!-- Provider and Cost -->
+          <div v-for="(provider, index) in course.providers" :key="index" class="info-item">
+            <span class="key">Provider:</span>
+            <span class="value">{{ provider.name }}</span>
+            <span class="key">Cost:</span>
+            <span class="value">${{ provider.cost }}</span>
+          </div>
+          <div class="info-item">
+            <span class="key">Hours per week:</span>
+            <span class="value">{{ course.hours }}</span>
+          </div>
+          <div class="info-item">
+            <span class="key">Related Certifications:</span>
+            <span class="value">{{ course.certification }}</span>
           </div>
         </div>
-        <div class="info-item-separator"></div>
-        <div class="info-box-right">
-          <div class="info-container">
-            <div class="info-item"><span class="key">Hours per week:</span> <span class="value">{{ course.hours
-                }}</span>
-            </div>
-            <div class="info-item"><span class="key">Related Certifications:</span> <span class="value">{{
-      course.certification }}</span></div>
-            <div class="info-item" v-for="(provider, index) in course.providers" :key="'cost' + index">
-              <span class="key">Cost:</span>
-              <span class="value">${{ provider.cost }}</span>
-            </div>
-
-          </div>
-
+        <div class="info-button">
+          <v-btn prepend-icon="mdi-cart-check" text="Order Course" type="apply" href="/forms"
+            variant="outlined"></v-btn>
+          <v-btn @click="toggleFavorite" :disabled="isFavorite"
+            :prepend-icon="isFavorite ? 'mdi-heart-off-outline' : 'mdi-heart'">
+            {{ isFavorite ? 'Remove Favorite' : 'Add to Favorites' }}
+          </v-btn>
         </div>
-
-      </div>
-      <div class="info-item"><span class="key"><v-btn prepend-icon="mdi-cart-check" text="Order Course" type="apply" href="/forms"
-            variant="outlined"></v-btn></span>
-        <span class="value"><v-btn @click="toggleFavorite" :disabled="isFavorite" :prepend-icon="isFavorite ? 'mdi-heart-off-outline' : 'mdi-heart'">
-  {{ isFavorite ? 'Remove Favorite' : 'Add Favorite' }}
-</v-btn>
- </span>
-      </div>
-
-
-      <div class="course-image">
-        <img src="/images/AWS.png" alt="AWS Course Image">
+        <figure class="course-image">
+          <img src="/images/AWS.png" alt="AWS Course Image">
+        </figure>
       </div>
     </div>
-    <div class="course-description">{{ course.description }}</div>
-  </div>
+  </main>
 </template>
+
 
 <script>
 //TODO: remove the console debugging lines before deploying our project.
@@ -114,9 +108,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-/* The items, have 2 columns to break up the information
- */
- @media screen and (max-width: 479px) {
+.course-image img {
+  /* To reduce the size of the image */
+  max-width: 60%;
+  height: auto;
+  display: block;
+  margin: 10px auto;
+}
+
+@media screen and (max-width: 479px) {
   .info-item {
     /* Margin for the key+value pairs */
     margin-right: 10px;
@@ -127,45 +127,16 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 10px;
+    margin-top: 20px;
   }
 
-  .info-button v-btn {
-    margin-bottom: 5px; /* Add some bottom margin for spacing between buttons */
-  }
 
-  /* To ensure space for each key+value pair */
-  .key {
-    display: block;
-  }
-
-  .value {
-    display: block;
-  }
-
-  /* Center the image */
   .course-image {
     text-align: center;
+  }
 }
-
-}
-
- .v-btn {
-  background-image: linear-gradient(to right, rgb(var(--v-theme-gradiantOne)), rgb(var(--v-theme-gradiantTwo)));
-  color: rgb(var(--v-theme-background));
-
-}
-
-.session-date {
-  text-align: center;
-  font-size: 1.2em;
-}
-
-.grid-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
+.course-image{
+  
 }
 
 .course-title {
@@ -173,64 +144,30 @@ export default {
   font-size: 1.5em;
 }
 
-.content-container {
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-}
-
-.course-info {
-  display: grid;
-  gap: 10px;
-  width: 100%;
-  grid-template-columns: minmax(0, 1fr) calc(10vw - 80px) minmax(0, 1fr);
-}
-
-.info-item-separator {
-  grid-row: 1;
-}
-
-.info-box-left {
-  display: flex;
-  grid-row: 1;
-  justify-content: end;
-}
-
-.info-box-right {
-  display: flex;
-  grid-row: 1;
-  justify-content: start;
-}
-
 .info-container {
-  display: inline-flex;
+  display: flex;
   flex-direction: column;
 }
 
 .info-item {
   display: flex;
-  justify-content: flex-start;
+  align-items: center;
+  justify-content: center;
+  margin: 5px 5px 5px 5px;
+}
+
+.key,
+.value {
+  margin-right: 8px;
 }
 
 .key {
   font-weight: bold;
-  margin-right: 8px;
-  /* Adjust the space between key and value */
 }
 
-.course-title,
-.course-description {
-  margin-top: 5px;
-  width: 100%;
-  text-align: center;
-}
+.v-btn {
+  background-image: linear-gradient(to right, rgb(var(--v-theme-gradiantOne)), rgb(var(--v-theme-gradiantTwo)));
+  color: rgb(var(--v-theme-background));
 
-.course-image img {
-
-  min-width: 30%;
-  max-width: 50%;
-  height: auto;
 }
 </style>
