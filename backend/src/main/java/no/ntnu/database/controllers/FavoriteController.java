42ca12ced530a4f43df8520be2540f0c722fd71c
emmanes <Emma.nesvik@hotmail.com>
2024-05-14 10:23:56 +0200

    Added swagger for favorite controller
    In discussion with Henrik, I added this while he will work on something else.

--- backend/src/main/java/no/ntnu/database/controllers/FavoriteController.java
+++ backend/src/main/java/no/ntnu/database/controllers/FavoriteController.java
@@ -1,5 +1,7 @@
 package no.ntnu.database.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import no.ntnu.database.entities.Favorite;
 import no.ntnu.database.entities.User;
 import no.ntnu.database.services.CourseService;
@@ -66,6 +68,9 @@ public class FavoriteController {
 	 *
 	 * @return All favorite entries for the authenticated user
 	 */
+	@Operation(summary = "Get all favorites",
+			description = "Gets all the favorites for the specific user")
+	@ApiResponse(responseCode = "200", description = "All favorites returned")
 	@GetMapping
 	public Iterable<Favorite> getAllFavorites() {
 		return favoriteService.getAllFavourites(getUser().getId());
@@ -76,6 +81,7 @@ public class FavoriteController {
 	 *
 	 * @param courseId The id of the {@link no.ntnu.database.entities.Course Course}
 	 *                 in the favorite entry
+	 *
 	 * @return {@link ResponseEntity} object containing either:
 	 *     <ul>
 	 *         <li>A favorite entry for the authorized user with course that matches id,
@@ -83,6 +89,10 @@ public class FavoriteController {
 	 *         <li>If no match is found, returns status 404.</li>
 	 *     </ul>
 	 */
+	@Operation(summary = "Get favorite",
+			description = "Get a specific favorite through the specified course id")
+	@ApiResponse(responseCode = "200", description = "Entry found")
+	@ApiResponse(responseCode = "404", description = "Entry not found")
 	@GetMapping("/{courseId}")
 	public ResponseEntity<Favorite> getFavorite(@PathVariable int courseId) {
 		return favoriteService.findById(courseId, getUser().getId())
@@ -91,12 +101,17 @@ public class FavoriteController {
 	}
 
 	/**
-	 * Adds a favorite entry for a the authorized {@link User}.
+	 * Adds a favorite entry for the authorized {@link User}.
 	 *
 	 * @param courseId The ID of the {@link no.ntnu.database.entities.Course Course}
 	 *                 to include in the entry
+	 *
 	 * @return 201 CREATED status on success, 400 BAD REQUEST on error
 	 */
+	@Operation(summary = "Add favorite",
+			description = "Adds a course to the favorite list")
+	@ApiResponse(responseCode = "201", description = "Favorite entry successfully added")
+	@ApiResponse(responseCode = "400", description = "Bad request, entry not added")
 	@PostMapping("/{courseId}")
 	public ResponseEntity<Favorite> add(@PathVariable int courseId) {
 		return courseService
@@ -115,8 +130,13 @@ public class FavoriteController {
 	 *
 	 * @param courseId The id of the {@link no.ntnu.database.entities.Course Course}
 	 *                 in the entry to remove
+	 *
 	 * @return 200 OK status on success, 404 NOT FOUND on error
 	 */
+	@Operation(summary = "Delete favorite",
+			description = "Deletes a favorite entry from the favorite list")
+	@ApiResponse(responseCode = "200", description = "Successfully removed entry")
+	@ApiResponse(responseCode = "404", description = "Entry not found")
 	@DeleteMapping("/{courseId}")
 	public ResponseEntity<String> delete(@PathVariable int courseId) {
 		return favoriteService.delete(courseId, getUser().getId())
