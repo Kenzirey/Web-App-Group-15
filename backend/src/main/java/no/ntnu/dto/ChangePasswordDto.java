73e7bc9b071153f656efebe9c5b30a5392a8b966
bratseth1 <52723913+bratseth1@users.noreply.github.com>
2024-05-20 01:40:18 +0200

    Password change dto

---
+++ backend/src/main/java/no/ntnu/dto/ChangePasswordDto.java
@@ -0,0 +1,25 @@
+package no.ntnu.dto;
+
+/**
+ * Data Transfer Object (DTO) for changing a user's password.
+ */
+public class ChangePasswordDto {
+    private String currentPassword;
+    private String newPassword;
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
